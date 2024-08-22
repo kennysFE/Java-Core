af5d1fe374528d9ee6e4996f88477741c02349df
hoangFE <metmoi556@email.com>
2024-08-22 22:28:56 +0700

    Practice file

--- src/lab10/FileClass.java
+++ src/lab10/FileClass.java
@@ -4,47 +4,60 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileClass {
 	public static void main(String[] args) {
-		File file = new File("example.txt");
+		Scanner scanner = new Scanner(System.in);
+
+		// Khởi tạo file
+
+		File file = new File("exampleiostream");
 
 		try {
+			// Kiem tra file ton tai
 			if (file.exists()) {
-				System.out.println(" File exist " + file.getName());
+				System.out.println(" File exist" + file.getName());
 				file.delete();
 			} else {
 				file.createNewFile();
-				System.out.println("File craete succeesfully");
+				System.out.println(" File craete succeesfully ");
 			}
 		} catch (IOException e) {
-			System.out.println("Loi file");
+			System.out.println(" Error check file exist !");
 			// In lỗi ra màn hình console
-			e.printStackTrace();
+			e.getStackTrace();
 		}
 
 		// Ghi File
 		FileOutputStream fos = null;
+
 		try {
 			// Create Connect to File (Tao mot ket noi toi file)
 			fos = new FileOutputStream(file);
 
-			// Data add on file
-			String str = "Input Data";
+			while (true) {
+				// Data add on file
+				System.out.println("Enter your data ");
+				String str = scanner.nextLine();
 
-			// Chuyen doi chuoi thanh mang byte, ma hoa thanh utf8
+				// Chuyen doi chuoi thanh mang byte, ma hoa thanh utf8
+				byte[] strbyte = str.getBytes("utf8");
 
-			byte[] data;
-			data = str.getBytes("utf8");
+				// Ghi mang byte vao file
+				fos.write(strbyte);
 
-			// Ghi mang byte vao file
-
-			fos.write(data);
+				System.out.println("Countinous ? [Y/n]");
+				String choice = scanner.nextLine();
+				if (choice.equals("n") || choice.equals("N")) {
+					break;
+				}
+			}
 
 		} catch (IOException e) {
-			System.out.println("Them du lieu vao file ko thanh cong");
+			System.out.println(" Error Wrriting data on file ");
 			// In lỗi ra màn hình console
-			e.printStackTrace();
+			e.getStackTrace();
 		} finally {
 
 			// Neu file khong co du lieu them vao => khong dong ket noi toi file
@@ -52,16 +65,18 @@ public class FileClass {
 				try {
 					fos.close();
 				} catch (IOException e2) {
-					System.out.println("Loi them du lieu va file");
-					e2.printStackTrace();
+					System.out.println("Closed file not successfully");
+					e2.getStackTrace();
 				}
 			}
 		}
 
 		// Doc File
+
 		FileInputStream fis = null;
 
-		// Gep chui
+		// Ghep chuoi
+
 		StringBuilder stringbuilder = new StringBuilder();
 
 		try {
@@ -70,29 +85,39 @@ public class FileClass {
 
 			// Tao bien lay gia tri khi file doc du lieu
 			int code;
+
 			while ((code = fis.read()) != -1) {
 				// Chuyen doi tui byte sang ki tu
 				char c = (char) code;
 
 				// Gep tung ki tu lai voi nhau => du lieu ban dau
 				stringbuilder.append(c);
+
 			}
 
-			System.out.println(stringbuilder);
+			// Hien chuoi ra man hinh
+			System.out.println(" Show Data : " + stringbuilder.toString());
 
 		} catch (Exception e) {
-			System.out.println("Doc file ko thanh cong");
-			e.printStackTrace();
+			// Doc file khong thanh cong
+			System.out.println("Reading data on file not successfully");
+			// Xuat hien loi ra man hinh
+			e.getStackTrace();
+
 		} finally {
+
 			if (fis != null) {
 				try {
 					fis.close();
 				} catch (Exception e2) {
-					System.out.println(" Doc file khong thanh cong ");
-					e2.printStackTrace();
+					// Doc file khong thanh cong
+					System.out.println(" Closed file not successfully ");
+					// Xuat hien loi ra man hinh
+					e2.getStackTrace();
 				}
 			}
 		}
 
+		scanner.close();
 	}
 }
