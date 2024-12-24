1a1777cf403b11763968657b326c2abd3a7e2e20
kennysFE <metmoi556@gmail.com>
2024-12-24 18:02:52 +0700

    update

--- src/AlgorithmJava/ArrayNumberJava.java
+++ src/AlgorithmJava/ArrayNumberJava.java
@@ -8,14 +8,37 @@ public class ArrayNumberJava {
 	public static void main(String[] args) {
 
 		// Ex1: 1,-3,5,-7,9,-11
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(" Ex1: Enter max value n = ? ");
+		int n = scanner.nextInt();
+		ArrayList<Integer> arrayPositiveNumber = new ArrayList<>();
+		ArrayList<Integer> arrayNumberChecked = new ArrayList<>();
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				arrayPositiveNumber.add(i);
+			}
+		}
+
+		System.out.println(" Array " + arrayPositiveNumber);
+
+		for (int i = 0; i < arrayPositiveNumber.size(); i++) {
+			if (arrayPositiveNumber != null && i % 2 == 0) {
+				arrayNumberChecked.add(arrayPositiveNumber.get(i) * -1);
+			} else {
+				arrayNumberChecked.add(arrayPositiveNumber.get(i));
+			}
+		}
+
+		System.out.println(" Array checked " + arrayNumberChecked);
 
 		// Ex2: 1,2, 3,5,8,13,21,...
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(" Enter max value n = ? ");
-		int n = scanner.nextInt();
+
+		System.out.println(" Ex2: Enter max value n = ? ");
+		int n2 = scanner.nextInt();
 
 		ArrayList<Integer> arrNumber = new ArrayList<>();
-		for (int i = 0; i <= n; i++) {
+		for (int i = 0; i <= n2; i++) {
 			if (arrNumber.size() < 2) {
 				arrNumber.add(i + 1);
 			} else {
@@ -23,7 +46,8 @@ public class ArrayNumberJava {
 			}
 		}
 
-		System.out.println(arrNumber.toString());
+		System.out.println(" Ex2: New Array " + arrNumber.toString());
+
 		scanner.close();
 	}
 }
---
+++ src/AlgorithmJava/SwapTwoNumber.java
@@ -0,0 +1,15 @@
+package AlgorithmJava;
+
+public class SwapTwoNumber {
+	public static void main(String[] args) {
+		// Ex1: Swap two number not using variable thrild
+		int a = 10;
+		int b = 20;
+		a = a + b; // a = 30
+		b = a - b; // b = 10
+		a = a - b; // a = 20
+
+		System.out.println(" a = " + a + " b = " + b);
+
+	}
+}
